326634163483a6c6ec4faee992baa184ee2c76da
Vineet Tiwari <40148275+ivineettiwari@users.noreply.github.com>
2024-12-25 12:54:35 +0530

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
 ]
 
 ROOT_URLCONF = 'test_project.urls'
@@ -135,6 +136,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
